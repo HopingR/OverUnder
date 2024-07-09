d7189a405af252abd7fd3afb32774d67bb5c410a
HopingR <hopingr7@gmail.com>
2024-07-08 21:00:09 -0400

    file

--- main.py
+++ main.py
@@ -1,33 +1,43 @@
-from game import Game
-from team import Team
-from helperfunctions import get_seasons
-import pandas as pd
-from sklearn.linear_model import LinearRegression
-
+from simpleDecisionTree import DecisionTree
+from datahandler import DataHandler
+from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
-    num_years = 5
-    
-    Team.team_stats_to_csv(num_years=num_years)
-    game_logs = Game.get_game_logs(num_years=num_years)
-    game_metrics = Game.get_team_metrics_for_games(game_logs)
-    
+
+    dh = DataHandler()
+    dh.load_data()
+    dh.set_X_y()
+
+
     while True:
-            cur_season = get_seasons()[0]
-            
-            team_away = Team.set_team(season=cur_season, name=input("Away Team: ").lower())
-            team_home = Team.set_team(season=cur_season, name=input("Home Team: ").lower())
+        # Get the dataframe for two teams with the dataframe formatted for prediction
+        X = dh.X
+        y = dh.y
+        
+        team_away_name = input("Away team: ")
+        team_home_name = input("Home team: ")
 
-            columns_to_drop = ["TEAM_ID_A", "TEAM_ID_H", "GP_A", "GP_H", "W_A", "W_H", "L_A", "L_H", "SEASON_A", "SEASON_H", "TOTAL_POINTS"]
+        try:
+            overunder_line = float(input("Vegas line: "))
+        except:
+            print("Bad Input\n")
+            continue
+        
+        X_pred = dh.get_X_pred(team_away_name=team_away_name, team_home_name=team_home_name)
+        
+        # dh.get_X_pred() will return false if bad input
+        if (X_pred is False):
+            print("Bad Input\n")
+            continue
+        
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+        decisionTree = DecisionTree()
+        decisionTree.fit(X, y)
 
-            X = game_metrics.drop(columns=columns_to_drop)
-            y = game_metrics["TOTAL_POINTS"]
+        decisionTree_score = decisionTree.get_score(X_test, y_test)
 
-            model = LinearRegression().fit(X, y)
+        y_pred = decisionTree.predict(X_pred)
 
-            X_pred = pd.concat([team_away.stats.add_suffix("_A"), team_home.stats.add_suffix("_H")], axis=1).drop(columns=columns_to_drop[:-1])
-            y_pred = model.predict(X_pred)
-
-            print(f"{team_away.info.nickname[0].capitalize()} vs {team_home.info.nickname[0].capitalize()}: {y_pred[0]:.2f} points\n")
-            
-            # p value analysis and look at simple decision tree, if around 3000 datapoints then can experiment neural network
+        print(f"{dh.team_away.info.nickname[0].capitalize()} vs {dh.team_home.info.nickname[0].capitalize()}: {y_pred[0]:.2f} points")
+        print(f"Percent dif: {(y_pred[0]/overunder_line-1)*100:.2f}%")
+        print(f"Simple Decision Tree MSE: {decisionTree_score:.2f}\n")   
